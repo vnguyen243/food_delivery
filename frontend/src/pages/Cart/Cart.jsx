b1dfa258391ccef8ddb336a1195891a283b41688
Luong Tuan Dat Nguyen <nltdat24@students.cs.ubc.ca>
2026-02-21 15:24:46 -0800

    Create and add functionalities to Cart page

--- frontend/src/pages/Cart/Cart.jsx
+++ frontend/src/pages/Cart/Cart.jsx
@@ -1,12 +1,74 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './Cart.css'
-
+import { StoreContext } from '../../context/StoreContext'
 const Cart = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+
+    const { cartItems, food_list, removeFromCart, getTotalCartAmount} = useContext(StoreContext);
+
+    return (
+        <div className='cart'>
+            <div className="cart-items">
+                <div className="cart-items-title">
+                    <p>Items</p>
+                    <p>Title</p>
+                    <p>Price</p>
+                    <p>Quantity</p>
+                    <p>Total</p>
+                    <p>Remove</p>
+                </div>
+                <br />
+                <hr />
+                {food_list.map((item, index) => {
+                    if (cartItems[item._id] > 0) {
+                        return (
+                            <dir>
+                                <div className="cart-items-title cart-items-item">
+                                    <img src={item.image} alt="" />
+                                    <p>{item.name}</p>
+                                    <p>{item.price} CAD</p>
+                                    <p>{cartItems[item._id]}</p>
+                                    <p>{item.price * cartItems[item._id]} CAD</p>
+                                    <p onClick={() => removeFromCart(item._id)} className='cross'>x</p>
+                                </div>
+                                <hr />
+                            </dir>
+                        )
+                    }
+                })}
+            </div>
+            <div className="cart-bottom">
+                <div className="cart-total">
+                    <h2>Cart Totals</h2>
+                    <div>
+                        <div className="cart-total-details">
+                            <p>Sutotal</p>
+                            <p>{getTotalCartAmount()} CAD</p>
+                        </div>
+                        <hr />
+                        <div className="cart-total-details">
+                            <p>Delivery Fee</p>
+                            <p>{2} CAD</p>
+                        </div>
+                        <hr />
+                        <div className="cart-total-details">
+                            <p>Total</p>
+                            <b>{getTotalCartAmount()+2} CAD</b>
+                        </div>
+                        <button>PROCEED TO CHECKOUT</button>
+                    </div>
+                </div>
+                <div className="cart-promocode">
+                    <div>
+                        <p>If you have a promo code, enter it here</p>
+                        <div className="cart-promocode-input">
+                            <input type="text" placeholder='PROMO CODE' />
+                            <button>Submit</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Cart
